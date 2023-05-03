c019ac2e1fd807a5e25a68615564e24d5de5d475
Emma M. Neil <emmaneil722@gmail.com>
2023-05-03 09:37:27 -0400

    truncation logic, hw5

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,7 @@
 import csv
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from collections import defaultdict
 
 '''
 Citation:
@@ -30,12 +31,14 @@ print(model.config.id2label[predicted_class_id])
 # Load tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
 model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
+sentiment_dict = defaultdict(int)
 
 def get_sentiment(text: str):
-    inputs = tokenizer(text, return_tensors="pt")
+    inputs = tokenizer(text, padding='max_length', truncation=True, max_length = 514, return_tensors="pt")
     with torch.no_grad():
         logits = model(**inputs).logits
     predicted_class_id = logits.argmax().item()
+    sentiment_dict[model.config.id2label[predicted_class_id]] += 1
     return model.config.id2label[predicted_class_id]
 
 def add_sentiment(input_file: str, output_file: str) -> None:
@@ -53,12 +56,26 @@ def add_sentiment(input_file: str, output_file: str) -> None:
         # Process and write rows
         for row in reader:
             text = row[1]
-            print(len(text))
-            sentiment = get_sentiment(text)
-            row.append(sentiment)
-            writer.writerow(row)
 
-# TODO: truncate input to a max size
+            max_lengths = [500, 1000, 1500, 1750, 2000, len(text)]
+            num_tries = len(max_lengths)-1
+            while num_tries > 0:
+                try:
+                    text = truncate(text, max_lengths[num_tries])
+                    print("Length: ", len(text))
+                    sentiment = get_sentiment(text)
+                    break
+                except:
+                    num_tries -= 1
+
+            if sentiment: # in case we don't get sentiment after trying
+                row.append(sentiment)
+                writer.writerow(row)
+            
+
+def truncate(output_string: str, max_length) -> list:
+    return output_string[0:max_length]
 
 add_sentiment("dataset_bbc/train.csv", "dataset_bbc/train_w_sentiment.csv")
-add_sentiment("dataset_bbc/test.csv", "dataset_bbc/test_w_sentiment.csv")
+print(sentiment_dict)
+# add_sentiment("dataset_bbc/test.csv", "dataset_bbc/test_w_sentiment.csv")
