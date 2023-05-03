dfe50034f48d8a66b986eb1d787aed5d7e082cf3
Emma M. Neil <emmaneil722@gmail.com>
2023-05-02 21:19:50 -0400

    new sentiment models

--- sentiment_1.py
+++ sentiment_1.py
@@ -1,9 +1,9 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-tokenizer = AutoTokenizer.from_pretrained("siebert/sentiment-roberta-large-english")
+tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
 
-model = AutoModelForSequenceClassification.from_pretrained("siebert/sentiment-roberta-large-english")
+model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
 
 inputs = tokenizer("Hello, my dog is stupid. sad", return_tensors="pt")
 
