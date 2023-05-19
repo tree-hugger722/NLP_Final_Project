642bdd07a6b26240a52c99c3a937292c20bbb7c3
Emma Neil <emn3@williams.edu>
2023-05-19 11:48:39 -0400

    cleaned up repo

--- sentiment_2.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-tokenizer = AutoTokenizer.from_pretrained("Seethal/sentiment_analysis_generic_dataset")
-
-model = AutoModelForSequenceClassification.from_pretrained("Seethal/sentiment_analysis_generic_dataset")
